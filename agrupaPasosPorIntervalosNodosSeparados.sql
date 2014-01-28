cdd548fdf59791f499dec9ff86c8542f3e58a776
Antonio Fernández Ares <antares.es@gmail.com>
2014-01-28 16:18:38 +0100

    Añadidos nuevos campos a la consulta

--- agrupaPasosPorIntervalosNodosSeparados.sql
+++ agrupaPasosPorIntervalosNodosSeparados.sql
@@ -1,8 +1,9 @@
-USE `sipesca`;
-DROP procedure IF EXISTS `agrupaPasosPorIntervalosNodosSeparados`;
-
+-- --------------------------------------------------------------------------------
+-- Routine DDL
+-- Note: comments before and after the routine body will not be stored by the server
+-- --------------------------------------------------------------------------------
 DELIMITER $$
-USE `sipesca`$$
+
 CREATE DEFINER=`root`@`localhost` PROCEDURE `agrupaPasosPorIntervalosNodosSeparados`(in fechaMIN Varchar(40), in fechaMAX Varchar(40), in intervalo INT)
 BEGIN
 DECLARE inter INT DEFAULT intervalo*1000*60;
@@ -10,18 +11,17 @@ DECLARE corte INT DEFAULT 19;
 
 IF intervalo >= 1440 THEN SET corte = 10; END IF;
 
-SELECT SUBSTR(FROM_UNIXTIME(truncate(tinicio/inter,0)*inter/1000),1,corte) as Fecha, idNodo ,  count(*)
-	FROM paso
+SELECT SUBSTR(FROM_UNIXTIME(truncate(tinicio/inter,0)*inter/1000),1,corte) as Fecha, idNodo ,  count(*) as Total, latitud,longitud,nombre,poligono
+	FROM paso, nodo
 		WHERE
 			tinicio
 			BETWEEN  UNIX_TIMESTAMP(fechaMIN)*1000
 				AND  UNIX_TIMESTAMP(fechaMAX)*1000
-		
+		    AND
+            paso.idNodo = nodo.idNodo
 	GROUP BY
 		idNodo,
 		truncate(tinicio/inter,0)
-	ORDER BY Fecha;
+	ORDER BY Fecha ASC;
  
-END$$
-
-DELIMITER ;
+END
